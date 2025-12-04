e3b005c3442a28355b37ab6b60298e27d0db973e
drg0n <adrien.bernard@edu.univ-fcomte.fr>
2025-12-04 12:53:43 +0100

    Update quasar.config.js

--- quasar.config.js
+++ quasar.config.js
@@ -45,7 +45,7 @@ export default defineConfig((ctx) => {
 
       // rebuildCache: true, // rebuilds Vite/linter/etc cache on startup
 
-      publicPath: '/FractalViz/',
+      publicPath: '/',
       // analyze: true,
       // env: {},
       // rawDefine: {}
